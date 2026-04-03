99dfdf4262bee9bd29f6397b501b12895bbf2536
haruka291 <haru.sky291@gmail.com>
2026-04-03 13:50:15 +0900

    Change message

--- 02_Basicset_IO/02_Distance/02_Distance.ino
+++ 02_Basicset_IO/02_Distance/02_Distance.ino
@@ -22,8 +22,8 @@ void loop() {
   Distance = Duration*340*100/1000000; //Set sonic speed to 340 m/s
 
   //Look at the serial monitor
-  Serial.print("Distance:");
   Serial.print(Distance);
-  Serial.println("cm");
+  Serial.print("cm");
+  Serial.println(":Distance");
   delay(500);
 }
