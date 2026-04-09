d08122622ea5cd096a344f915a415617773fed0d
haruka291 <haru.sky291@gmail.com>
2026-04-09 13:36:40 +0900

    my original melody

--- 02_Basicset_IO/09_Speaker/09_Speaker.ino
+++ 02_Basicset_IO/09_Speaker/09_Speaker.ino
@@ -1,5 +1,6 @@
 const int spkrPin = 3; //3:ConnectorA 4:ConnectorB
 
+#define SI_L 246.9 // 低いシ
 #define DO 261.6
 #define _DO 277.18
 #define RE 293.665
@@ -11,48 +12,48 @@ const int spkrPin = 3; //3:ConnectorA 4:ConnectorB
 #define _SO 415.305
 #define RA 440
 #define _RA 466.164
-#define TI 493.883
+#define SI 493.9
 #define octDO 523.251
 
 
 void playmusic(){
-  // き(ド)
-  ledcWriteTone(spkrPin, DO);
-  delay(350);
-  ledcWriteTone(spkrPin, 0); delay(50); // 音の切れ目
+//
+  ledcWriteTone(spkrPin, MI); delay(350);
+  ledcWriteTone(spkrPin, 0);  delay(50);
+  ledcWriteTone(spkrPin, MI); delay(350);
+  ledcWriteTone(spkrPin, 0);  delay(50);
+  ledcWriteTone(spkrPin, FA); delay(350);
+  ledcWriteTone(spkrPin, 0);  delay(50);
+  ledcWriteTone(spkrPin, SO); delay(350);
+  ledcWriteTone(spkrPin, 0);  delay(50);
 
-  // ら(ド)
-  ledcWriteTone(spkrPin, DO);
-  delay(350);
-  ledcWriteTone(spkrPin, 0); delay(50);
-
-  // き(ソ)
-  ledcWriteTone(spkrPin, SO);
-  delay(350);
-  ledcWriteTone(spkrPin, 0); delay(50);
-
-  // ら(ソ)
-  ledcWriteTone(spkrPin, SO);
-  delay(350);
-  ledcWriteTone(spkrPin, 0); delay(50);
-
-  // ひ(ラ)
-  ledcWriteTone(spkrPin, RA);
-  delay(350);
-  ledcWriteTone(spkrPin, 0); delay(50);
-
-  // か(ラ)
-  ledcWriteTone(spkrPin, RA);
-  delay(350);
-  ledcWriteTone(spkrPin, 0); delay(50);
-
-  // る(ソ〜)
-  ledcWriteTone(spkrPin, SO);
-  delay(750); // 最後の音は2倍の長さで伸ばす
   
-  // 次のフレーズへの間（無音）
-  ledcWriteTone(spkrPin, 0); 
-  delay(400);
+  ledcWriteTone(spkrPin, SO); delay(350);
+  ledcWriteTone(spkrPin, 0);  delay(50);
+  ledcWriteTone(spkrPin, FA); delay(350);
+  ledcWriteTone(spkrPin, 0);  delay(50);
+  ledcWriteTone(spkrPin, MI); delay(350);
+  ledcWriteTone(spkrPin, 0);  delay(50);
+  ledcWriteTone(spkrPin, RE); delay(350);
+  ledcWriteTone(spkrPin, 0);  delay(50);
+
+  
+  ledcWriteTone(spkrPin, DO); delay(350);
+  ledcWriteTone(spkrPin, 0);  delay(50);
+  ledcWriteTone(spkrPin, DO); delay(350);
+  ledcWriteTone(spkrPin, 0);  delay(50);
+  ledcWriteTone(spkrPin, RE); delay(350);
+  ledcWriteTone(spkrPin, 0);  delay(50);
+  ledcWriteTone(spkrPin, MI); delay(350);
+  ledcWriteTone(spkrPin, 0);  delay(50);
+
+  
+  ledcWriteTone(spkrPin, MI); delay(500); 
+  ledcWriteTone(spkrPin, 0);  delay(50);
+  ledcWriteTone(spkrPin, RE); delay(200); 
+  ledcWriteTone(spkrPin, 0);  delay(50);
+  ledcWriteTone(spkrPin, RE); delay(750); 
+  ledcWriteTone(spkrPin, 0);  delay(400);
   }
 
 void setup() {
